b286b2f9669649983e2a5124c71633a19feb8157
Andre <andremotta96@gmail.com>
2022-05-13 18:18:09 -0300

    Adding carrousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './App.css'
+import Logo from './assets/logo.png'
+import Nome from './assets/nome.png'
+import Face from './assets/face.png'
+import Insta from './assets/insta.png'
+import Twitter from './assets/twitter.png'
+import Base from './assets/image_base.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className='Header' >
+        <img className='Logo' src={Logo} alt ='' />
+        <img className='Nome' src={Nome} alt='' />
+        <div className='Redes'>
+          <a className='Rede'><img src={Face} alt='' className='Face'/></a>
+          <a className='Rede'><img src={Insta} alt='' className='Face'/></a>
+          <a className='Rede'><img src={Twitter} alt='' className='Face'/></a>
+        </div>
+      </div>
+      <Carousel showStatus={false} showIndicators={false} showThumbs={false}>
+        <div>
+            <img src={Base} />
+            <p className="legend">Legend 1</p>
+        </div>
+        <div>
+            <img src={Base} />
+            <p className="legend">Legend 2</p>
+        </div>
+        <div>
+            <img src={Base} />
+            <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     </div>
   )
 }
